df1a305a0a1216e9c84ba161f5c677484b75f9bd
Dilandds <e17379@eng.pdn.ac.lk>
2023-02-03 13:52:40 +0530

    completed new game functionality

--- script.js
+++ script.js
@@ -37,12 +37,16 @@ const clickRoll = function () {
     count1 += random;
     document.getElementById(`current--${activePlayer}`).textContent = count1;
   } else {
+    //when get 1
     count1 = 0;
     document.getElementById(`current--${activePlayer}`).textContent = count1;
+    //remove the active color from current player
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.remove('player--active');
+    //change the active player
     activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //give him the active player color
     document
       .querySelector(`.player--${activePlayer}`)
       .classList.add('player--active');
@@ -58,12 +62,42 @@ const clickHold = function () {
   document.getElementById(`current--${activePlayer}`).textContent = count1;
 
   if (total[activePlayer] < 100) {
+    //chnage the total score of the active player
     document.getElementById(`score--${activePlayer}`).textContent =
       total[activePlayer];
+    //remove the active color from current player
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+    //change active player if he did not win
     activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //give  the new active player color
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--active');
   } else {
     console.log(activePlayer + 'Wins');
   }
 };
 
 btnHold.addEventListener('click', clickHold);
+
+//new game functinality
+const clickNewGame = function () {
+  count1 = 0;
+  total = [0, 0];
+  document.getElementById(`current--0`).textContent = count1;
+  document.getElementById(`current--1`).textContent = count1;
+  diceEl.classList.add('hidden');
+
+  document.getElementById(`score--0`).textContent = 0;
+  document.getElementById(`score--1`).textContent = 0;
+  if (
+    document.querySelector('.player--1').classList.contains('player--active')
+  ) {
+    document.querySelector(`.player--1`).classList.remove('player--active');
+    document.querySelector(`.player--0`).classList.add('player--active');
+  }
+};
+
+btnNew.addEventListener('click', clickNewGame);
