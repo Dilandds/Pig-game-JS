59fcb893a321d3567937ba3ac575ad1351d08f0b
Dilandds <e17379@eng.pdn.ac.lk>
2023-02-03 14:11:42 +0530

    optimized the code

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 //selct elements
 const score1El = document.getElementById('score--0');
 const score2El = document.getElementById('score--1');
-// const curscore1El = document.getElementById('current--0');
-// const curscore2El = document.getElementById('current--1');
+const curscore1El = document.getElementById('current--0');
+const curscore2El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player1 = document.querySelector(`.player--0`);
+const player2 = document.querySelector(`.player--1`);
 
 //update elements to initial values
 score1El.textContent = 0;
@@ -41,15 +43,19 @@ const clickRoll = function () {
     count1 = 0;
     document.getElementById(`current--${activePlayer}`).textContent = count1;
     //remove the active color from current player
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-    //change the active player
+    // document
+    //   .querySelector(`.player--${activePlayer}`)
+    //   .classList.remove('player--active');
+    // //change the active player
+    // activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    // //give him the active player color
+    // document
+    //   .querySelector(`.player--${activePlayer}`)
+    //   .classList.add('player--active');
+
     activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    //give him the active player color
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--active');
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
   }
 };
 
@@ -84,19 +90,22 @@ btnHold.addEventListener('click', clickHold);
 
 //new game functinality
 const clickNewGame = function () {
+  //set current count to 0
   count1 = 0;
+  // set total counts to zero
   total = [0, 0];
-  document.getElementById(`current--0`).textContent = count1;
-  document.getElementById(`current--1`).textContent = count1;
+  //reset total counts
+  curscore1El.textContent = count1;
+  curscore2El.textContent = count1;
+  //make the dice hide
   diceEl.classList.add('hidden');
 
-  document.getElementById(`score--0`).textContent = 0;
-  document.getElementById(`score--1`).textContent = 0;
-  if (
-    document.querySelector('.player--1').classList.contains('player--active')
-  ) {
-    document.querySelector(`.player--1`).classList.remove('player--active');
-    document.querySelector(`.player--0`).classList.add('player--active');
+  score1El.textContent = 0;
+  score2El.textContent = 0;
+  //reset the active player to player 1
+  if (player2.classList.contains('player--active')) {
+    player1.classList.add('player--active');
+    player2.classList.remove('player--active');
   }
 };
 
